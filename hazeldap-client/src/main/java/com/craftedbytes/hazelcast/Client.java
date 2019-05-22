9fbcae5554a80eec457ac4458cbf09996900b980
m-david <manny.david@hazelcast.com>
2019-05-21 20:44:37 -0700

    fixed timeout issue

--- hazeldap-client/src/main/java/com/craftedbytes/hazelcast/Client.java
+++ hazeldap-client/src/main/java/com/craftedbytes/hazelcast/Client.java
@@ -14,47 +14,47 @@ import java.util.logging.Level;
 
 public class Client {
 
-//    private final ILogger logger = Logger.getLogger(getClass().getName());
+    private final ILogger logger = Logger.getLogger(getClass().getName());
 
-//    public static void main(String args[]){
-//
-//        Client client = new Client();
-//
-//        client.adminUserCanPutIntoImportantMap();
-//
-//        client.readOnlyUserCannotPutIntoImportantMap();
-//
-//    }
+    public static void main(String args[]){
 
-//    private void readOnlyUserCannotPutIntoImportantMap() {
-//
-//        HazelcastInstance readOnlyClient = getClientConnection("jbloggs", "password3", "127.0.0.1");
-//
-//        Map<String,String> readOnlyClientsImportantMap = readOnlyClient.getMap("importantMap");
-//
-//        // This will pass
-//        logger.log(Level.INFO,"-------------> Joe is performing get on the ImportantMap (Should Pass)");
-//        readOnlyClientsImportantMap.get("1");
-//
-//        // This will fail as Joe is not a member of the correct group.
-//        try{
-//            logger.log(Level.INFO,"-------------> Joe is performing put on the ImportantMap (Should Fail)");
-//            readOnlyClientsImportantMap.put("2","2");
-//        } catch (AccessControlException e){
-//            logger.log(Level.SEVERE,"Could not perform put operation, access denied",e);
-//        }
-//    }
+        Client client = new Client();
 
-//    private void adminUserCanPutIntoImportantMap() {
-//
-//        HazelcastInstance adminClient = getClientConnection("dbrimley", "password1", "127.0.0.1");
-//
-//        Map<String,String> adminClientsImportantMap = adminClient.getMap("importantMap");
-//
-//        // This will pass
-//        logger.log(Level.INFO,"-------------> David is performing put on the ImportantMap (Should Pass)");
-//        adminClientsImportantMap.put("1","1");
-//    }
+        client.adminUserCanPutIntoImportantMap();
+
+        client.readOnlyUserCannotPutIntoImportantMap();
+
+    }
+
+    private void readOnlyUserCannotPutIntoImportantMap() {
+
+        HazelcastInstance readOnlyClient = getClientConnection("jbloggs", "password3", "127.0.0.1");
+
+        Map<String,String> readOnlyClientsImportantMap = readOnlyClient.getMap("importantMap");
+
+        // This will pass
+        logger.log(Level.INFO,"-------------> Joe is performing get on the ImportantMap (Should Pass)");
+        readOnlyClientsImportantMap.get("1");
+
+        // This will fail as Joe is not a member of the correct group.
+        try{
+            logger.log(Level.INFO,"-------------> Joe is performing put on the ImportantMap (Should Fail)");
+            readOnlyClientsImportantMap.put("2","2");
+        } catch (AccessControlException e){
+            logger.log(Level.SEVERE,"Could not perform put operation, access denied",e);
+        }
+    }
+
+    private void adminUserCanPutIntoImportantMap() {
+
+        HazelcastInstance adminClient = getClientConnection("dbrimley", "password1", "127.0.0.1");
+
+        Map<String,String> adminClientsImportantMap = adminClient.getMap("importantMap");
+
+        // This will pass
+        logger.log(Level.INFO,"-------------> David is performing put on the ImportantMap (Should Pass)");
+        adminClientsImportantMap.put("1","1");
+    }
 
     public HazelcastInstance getClientConnection(String username, String password, String thisClientIP) {
         ClientConfig clientConfig = new ClientClasspathXmlConfig("hazelcast-client.xml");
